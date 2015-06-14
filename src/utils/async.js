430a13b8ac6dd3c62116b2d1f3be03dbbfe156b0
wernerdegroot <github@wernerdegroot.nl>
2015-06-14 20:51:41 +0200

    Type annotations for async.js

--- src/utils/async.js
+++ src/utils/async.js
@@ -1,6 +1,6 @@
 goog.provide('utils.async');
 
-/** @type {function(function(function(*): undefined, function(*): undefined): !angular.$q.Promise)} */
+/** @type {function(function(function(*): undefined, function(*): undefined): undefined): !angular.$q.Promise} */
 var promiseConstructor;
 
 function initializePromiseConstructor($q) {
@@ -17,12 +17,25 @@ utils.async = function async(generatorFunction) {
         
         // Call the generator function with the supplied arguments
         // and the right context (this). This will yield a generator.
+        /** @type {Generator.<?>} */
         var generator = generatorFunction.apply(this, arguments);
         
-        return promiseConstructor(function(/** function(*): undefined */ resolve, /** function(*): undefined */ reject) {
-            function resume(method, value) {
+        /**
+         * @param {function(RETURN_VALUE): undefined} resolve
+         * @param {function(RETURN_VALUE): undefined} reject
+         * @return {undefined}
+         * @template RETURN_VALUE
+         */
+        function promiseConstructorArg(resolve, reject) {
+            /**
+             * @param {function(Generator.<VALUE>, VALUE): {value:VALUE, done:boolean}} continueWithGenerator
+             * @param {VALUE} value
+             * @return {undefined}
+             * @template VALUE
+             */
+            function resume(continueWithGenerator, value) {
                 try {
-                    var result = generator[method](value);
+                    var result = continueWithGenerator(generator, value);
                     if (result.done) {
                         resolve(result.value);
                     } else {
@@ -32,9 +45,32 @@ utils.async = function async(generatorFunction) {
                     reject(e);
                 }
             }
-            var resumeNext = resume.bind(null, 'next');
-            var resumeThrow = resume.bind(null, 'throw');
+            
+            /**
+             * @param {Generator.<VALUE>} generator
+             * @param {VALUE} value
+             * @return {{value:VALUE, done:boolean}}
+             * @template VALUE
+             */
+            function getNextFromGenerator(generator, value) {
+                return generator.next(value);
+            };
+            
+            /**
+             * @param {Generator.<VALUE>} generator
+             * @param {VALUE} value
+             * @return {{value:VALUE, done:boolean}}
+             * @template VALUE
+             */
+            function throwFromGenerator(generator, value) {
+                return generator.throw(value);    
+            };
+            
+            var resumeNext = resume.bind(null, getNextFromGenerator);
+            var resumeThrow = resume.bind(null, throwFromGenerator);
             resumeNext(undefined);
-        });
+        }
+        
+        return promiseConstructor(promiseConstructorArg);
     };
 };
